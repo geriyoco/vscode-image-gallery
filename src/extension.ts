5b43b4df10ab2b2def6423ddab2fdf24bcbcf183
Fanurs Teh <fyanurs@gmail.com>
2022-03-26 14:54:41 -0400

    :poop: First minimally working gallery

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,58 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-image-gallery" is now active!');
+	console.log('Congratulations, extension "vscode-image-gallery" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-image-gallery.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Image Gallery!');
+	let disposable = vscode.commands.registerCommand('vscode-image-gallery.start', async () => {
+		const img_extensions = [
+			'jpg',
+			'jpeg',
+			'png',
+			'gif',
+			'webp',
+		]
+		const files = await vscode.workspace.findFiles('**/*.{' + img_extensions.join(',') + '}');
+		const img_paths = files.map(
+			file => vscode.Uri.file(file.fsPath)
+		);
+
+		const panel = vscode.window.createWebviewPanel(
+			'meowCoding',
+			'Meow Coding',
+			vscode.ViewColumn.One,
+
+			{
+				localResourceRoots: [vscode.Uri.file(context.extensionPath)]
+			}
+		)
+		const img_webviewUris = img_paths.map(
+			img_path => panel.webview.asWebviewUri(img_path)
+		);
+		panel.webview.html = getWebviewContent(img_webviewUris);
+
+		vscode.window.showInformationMessage('Image Gallery is now active!');
 	});
-
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
+function getWebviewContent(img_webviewUris: vscode.Uri[]) {
+	const img_html = img_webviewUris.map(
+		img => `<img src="${img}" style="height: 100px;">`
+	).join('\n');
+	console.log(img_html);
+
+	return `<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Meow Coding</title>
+</head>
+<body>
+	<h1>GRYC Image Gallery</h1>
+	${img_html}
+</body>
+</html>`;
+}
+
 export function deactivate() {}
