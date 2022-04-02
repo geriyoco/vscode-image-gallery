399fc4a67a6a96dc1191cdf8eab297d97e201100
Fanurs Teh <fyanurs@gmail.com>
2022-04-02 15:36:29 -0400

    :sparkles: Open image on right panel and refactored code

--- src/extension.ts
+++ src/extension.ts
@@ -1,85 +1,27 @@
 import * as vscode from 'vscode';
-import * as path from 'path';
+import * as main_panel from './main_panel';
+import * as viewer_panel from './viewer_panel';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, extension "vscode-image-gallery" is now active!');
 
 	let disposable = vscode.commands.registerCommand('vscode-image-gallery.start', async () => {
-		const panel = vscode.window.createWebviewPanel(
-			'imagegallery',
-			'Image Gallery',
-			vscode.ViewColumn.One,
-			{
-				localResourceRoots: [vscode.Uri.file(getCwd()), vscode.Uri.file(path.join(context.extensionPath, 'media'))],
-				enableScripts: true
-			}
+		const mainPanel = await main_panel.createPanel(context);
+		
+		mainPanel.webview.onDidReceiveMessage(
+			message => {
+				switch (message.command) {
+					case 'vscode-image-gallery.openImageViewer':
+						viewer_panel.createPanel(context, mainPanel.webview, message.src);
+						return;
+				}
+			},
+			undefined,
+			context.subscriptions,
 		);
-		const imgExtensions = [
-			'jpg',
-			'jpeg',
-			'png',
-			'gif',
-			'webp',
-		];
-		const files = await vscode.workspace.findFiles('**/*.{' + imgExtensions.join(',') + '}');
-		const imgPaths = files.map(file => vscode.Uri.file(file.fsPath));
-		const imgWebviewUris = imgPaths.map(
-			imgPath => panel.webview.asWebviewUri(imgPath)
-		);
-		panel.webview.html = getWebviewContent(context, panel.webview, imgWebviewUris);
 	});
+
 	context.subscriptions.push(disposable);
-}
-
-function getCwd() {
-	if (!vscode.workspace.workspaceFolders) {
-		let message = "Image Gallery: Working folder not found, open a folder an try again";
-		vscode.window.showErrorMessage(message);
-		return '';
-	}
-	var cwd = vscode.workspace.workspaceFolders[0].uri.path;
-	let message = `Image Gallery: folder: ${cwd}`;
-	vscode.window.showInformationMessage(message);
-	return cwd;
-}
-
-function getWebviewContent(context: vscode.ExtensionContext, webview: vscode.Webview, imgWebviewUris: vscode.Uri[]) {
-	const imgHtml = imgWebviewUris.map(img => `<img src="${img}" class="image lozad">`).join('\n');
-	const stylesGallery = webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, 'media', 'gallery.css'));
-	const scriptUri = vscode.Uri.joinPath(context.extensionUri, 'media', 'gallery.js').with({ 'scheme': 'vscode-resource' });
-	const nonce = getNonce();
-	return (
-		`<!DOCTYPE html>
-		<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}'; style-src ${webview.cspSource};">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			
-			<link href="${stylesGallery}" rel="stylesheet" />
-
-			<title>Image Gallery</title>
-		</head>
-		<body>
-			<div class="grid">
-				<div>
-					${imgHtml}
-				</div>
-			</div>
-			
-			<script nonce="${nonce}" src="${scriptUri}"></script>
-		</body>
-		</html>`
-	);
-}
-
-function getNonce() {
-	let text = '';
-	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	for (let i = 0; i < 32; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return text;
 }
 
 export function deactivate() {}
---
+++ src/main_panel.ts
@@ -0,0 +1,89 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as utils from './utils';
+
+export async function createPanel(context: vscode.ExtensionContext) {
+    const panel = vscode.window.createWebviewPanel(
+        'imagegallery',
+        'Image Gallery',
+        vscode.ViewColumn.One,
+        {
+            localResourceRoots: [
+                vscode.Uri.file(
+                    utils.getCwd()),
+                    vscode.Uri.file(path.join(context.extensionPath, 'media'),
+                ),
+            ],
+            enableScripts: true,
+        }
+    );
+
+    const imgPaths = await getImagePaths();
+    const imgWebviewUris = imgPaths.map(
+        imgPath => panel.webview.asWebviewUri(imgPath)
+    );
+    panel.webview.html = getWebviewContent(context, panel.webview, imgWebviewUris);
+
+    return panel;
+}
+
+export async function getImagePaths() {
+    const imgExtensions = [
+        'jpg',
+        'jpeg',
+        'png',
+        'gif',
+        'webp',
+    ];
+    const files = await vscode.workspace.findFiles('**/*.{' + imgExtensions.join(',') + '}');
+    const imgPaths = files.map(
+        file => vscode.Uri.file(file.fsPath)
+    );
+    return imgPaths;
+}
+
+export function getWebviewContent(
+    context: vscode.ExtensionContext,
+    webview: vscode.Webview,
+    imgWebviewUris: vscode.Uri[],
+) {
+	const imgHtml = imgWebviewUris.map(
+        img => `<img src="${img}" class="image lozad">`
+    ).join('\n');
+
+	const styleHref = webview.asWebviewUri(
+        vscode.Uri.joinPath(context.extensionUri, 'media', 'gallery.css')
+    );
+
+	const scriptUri = vscode.Uri.joinPath(
+        context.extensionUri, 'media', 'gallery.js'
+    ).with({
+        'scheme': 'vscode-resource'
+    });
+
+	const nonce = utils.getNonce();
+
+	return (
+		`<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}'; style-src ${webview.cspSource};">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			
+			<link href="${styleHref}" rel="stylesheet" />
+
+			<title>Image Gallery</title>
+		</head>
+		<body>
+			<div class="grid">
+				<div>
+					${imgHtml}
+				</div>
+			</div>
+			
+			<script nonce="${nonce}" src="${scriptUri}"></script>
+		</body>
+		</html>`
+	);
+}
