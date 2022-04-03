b90281ee41db1b58a6ea1f671ac25b25fd764ea4
Fanurs Teh <fyanurs@gmail.com>
2022-04-03 13:01:39 -0400

    :green_heart: Resolved zooming (#8) and panning (#4)
    Behaves almost identical to the "Photos" App in Windows 10/11.
    
    Both zooming and panning have to be handled simultaneously.

--- media/viewer.js
+++ media/viewer.js
@@ -1,114 +1,58 @@
-
 (function () {
-	const vscode = acquireVsCodeApi();
-	const imageElement = document.getElementById("image");
-	let zoom = 1;
-	const ZOOM_SPEED = 0.1;
+	// Zoom in/out with mouse wheel & Panning with mouse
+	// Reference: https://dev.to/stackfindover/zoom-image-point-with-mouse-wheel-11n3
 
-	imageElement.addEventListener("wheel", (event) => {
-		if (event.deltaY > 0) {
-			if (zoom < 0) {
-				zoom = 0;
-			}
-			if (zoom < 1) {
-				zoom -= ZOOM_SPEED * zoom;
-			} else {
-				zoom -= ZOOM_SPEED;
-			}
-		} else {
-			if (zoom < 1) {
-				zoom += ZOOM_SPEED / 3;
-			} else {
-				zoom += ZOOM_SPEED;
-			}
-		}
-		imageElement.style.transform = `scale(${zoom})`;
+	const container = document.getElementById('container');
+
+	let scale = 1;
+	let scrollRate = 1.1;
+	let isPanning = false;
+	let initialPoint = {x: 0, y: 0};
+	let point = {x: 0, y: 0};
+
+	function setTransform(translater, scalar) {
+		container.style.transform = `
+			translate(${translater.x}px, ${translater.y}px)
+			scale(${scalar})
+		`;
+	}
+
+	container.addEventListener("wheel", (event) => {
+		event.preventDefault();
+		let pointScale = {
+			x: (event.clientX - point.x) / scale,
+			y: (event.clientY - point.y) / scale
+		};
+		let delta = (event.wheelDelta ? event.wheelDelta : -event.deltaY);
+		(delta > 0) ? scale *= scrollRate : scale /= scrollRate;
+		point = {
+			x: event.clientX - pointScale.x * scale,
+			y: event.clientY - pointScale.y * scale
+		};
+		setTransform(point, scale);
 	});
 
-	var xCursorDiff = 0,
-		yCursorDiff = 0,
-		xImgElement = 0,
-		yImgElement = 0,
-		dragging = null,
-		currentCursorPosition = [0, 0],
-		lastCursorPosition = [0, 0];
-
-	imageElement.addEventListener('mousedown', (event) => {
-		dragging = true;
-		console.log(event);
-		// xImgElement = window.event.clientX - document.getElementById('image').offsetLeft;
-		xImgElement = imageElement.offsetLeft;
-		// yImgElement = window.event.clientY - document.getElementById('image').offsetTop;
-		yImgElement = imageElement.offsetTop;
-		currentCursorPosition = [window.event.clientX, window.event.clientY];
-		// if (currentCursorPosition[0] === 0 && currentCursorPosition[1] === 0) {
-		// 	currentCursorPosition = [window.event.clientX, window.event.clientY];
-		// } else {
-		// 	currentCursorPosition = [xCursorDiff, yCursorDiff];
-		// }
-		console.log(currentCursorPosition, xCursorDiff, yCursorDiff);
-		console.log(window.event.clientX, window.event.clientY, xImgElement, yImgElement);
-		// console.log(document.getElementById('container'));
+	container.addEventListener("mousedown", (event) => {
+		event.preventDefault();
+		isPanning = true;
+		initialPoint = {
+			x: event.clientX - point.x,
+			y: event.clientY - point.y
+		};
 	});
 
-	imageElement.addEventListener('mousemove', (event) => {
-		if (dragging === true) {
-			let xCursor = window.event.clientX;
-			let yCursor = window.event.clientY;
-			if (xCursorDiff !== 0 && yCursorDiff !== 0) {
-				xCursorDiff = currentCursorPosition[0] - xCursor + xCursorDiff;
-				yCursorDiff = currentCursorPosition[1] - yCursor + yCursorDiff;
-			} else {
-				xCursorDiff = currentCursorPosition[0] - xCursor;
-				yCursorDiff = currentCursorPosition[1] - yCursor;
-			}
-
-			if (imageElement !== null) {
-				imageElement.style.transform = `scale(${zoom}) translate(${-xCursorDiff}px, ${-yCursorDiff}px)`;
-				// imageElement.style.left = `scale(${zoom}) ${xImgElement}px`;
-				// imageElement.style.top = `scale(${zoom}) ${yImgElement}px`;
-				console.log(imageElement.style.transform, currentCursorPosition, xCursor, yCursor, xCursorDiff, yCursorDiff);
-			}
-		}
+	container.addEventListener("mousemove", (event) => {
+		event.preventDefault();
+		if (!isPanning) { return; }
+		point = {
+			x: event.clientX - initialPoint.x,
+			y: event.clientY - initialPoint.y
+		};
+		setTransform(point, scale);
 	});
 
-	imageElement.addEventListener('mouseup', (event) => {
-		dragging = false;
-		// lastCursorPosition = [xCursorDiff, yCursorDiff];
-		// currentCursorPosition = [xCursorDiff, yCursorDiff];
-		// console.log(currentCursorPosition);
+	container.addEventListener("mouseup", (event) => {
+		event.preventDefault();
+		isPanning = false;
 	});
-
-	// var img_ele = null,
-	// 	x_cursor = 0,
-	// 	y_cursor = 0,
-	// 	x_img_ele = 0,
-	// 	y_img_ele = 0;
-
-	// function start_drag() {
-	// 	img_ele = this;
-	// 	x_img_ele = window.event.clientX - document.getElementById('image').offsetLeft;
-	// 	y_img_ele = window.event.clientY - document.getElementById('image').offsetTop;
-
-	// }
-
-	// function stop_drag() {
-	// 	img_ele = null;
-	// }
-
-	// function while_drag() {
-	// 	var x_cursor = window.event.clientX;
-	// 	var y_cursor = window.event.clientY;
-	// 	if (img_ele !== null) {
-	// 		img_ele.style.left = (x_cursor - x_img_ele) + 'px';
-	// 		img_ele.style.top = (window.event.clientY - y_img_ele) + 'px';
-
-	// 		console.log(img_ele.style.left + ' - ' + img_ele.style.top);
-
-	// 	}
-	// }
-
-	// document.getElementById('image').addEventListener('mousedown', start_drag);
-	// document.getElementById('container').addEventListener('mousemove', while_drag);
-	// document.getElementById('container').addEventListener('mouseup', stop_drag);
-}());;
+}());
