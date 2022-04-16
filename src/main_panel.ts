24bbb44c7b97ff396e16600963fa6677d06389ca
Alex Chandra <alex2chandra@hotmail.com>
2022-04-17 01:57:49 +0800

    fix: naive persistence

--- src/main_panel.ts
+++ src/main_panel.ts
@@ -14,6 +14,7 @@ export async function createPanel(context: vscode.ExtensionContext, galleryFolde
                 ),
             ],
             enableScripts: true,
+            retainContextWhenHidden: true,
         }
     );
 
