e1e3601c56278e41b42e47320719dea0813d4bd8
Fanurs Teh <fyanurs@gmail.com>
2022-04-17 11:23:03 -0400

    :children_crossing: Zoom at center
    It turns out that the default panzoom behavior is already what I wanted.

--- media/viewer.js
+++ media/viewer.js
@@ -2,9 +2,6 @@
 	const container = document.getElementById('container');
 
 	panzoom(container, {
-		minZoom: 0.4,
-		bounds: true,
-		boundsPadding: 0.1,
-		transformOrigin: { x: 0.5, y: 0.5 }
+		minZoom: 0.6,
 	});
 }());
