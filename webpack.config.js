8e30f6a5beb409b6835621081788581fd094549a
Fanurs Teh <fyanurs@gmail.com>
2022-09-17 05:45:39 -0400

    :adhesive_bandage: To fix vsce package error
    https://github.com/microsoft/vscode-extension-telemetry/issues/41#issuecomment-598852991

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ const extensionConfig = {
     libraryTarget: 'commonjs2'
   },
   externals: {
-    vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    'applicationinsights-native-metrics': 'commonjs applicationinsights-native-metrics' // ignored because we don't ship native module
     // modules added here also need to be added in the .vscodeignore file
   },
   resolve: {
