b084185b4de02cc4b26af6f9f21b64d910747ae3
Fanurs Teh <fyanurs@gmail.com>
2022-09-17 17:38:29 -0400

    :children_crossing: Custom telemetry setting

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -19,6 +19,7 @@ export class ExtensionReporter extends TelemetryReporter {
     constructor(
         context: vscode.ExtensionContext,
         public verbose = false, // true for development; false for production
+        public readonly enableTelemetry: boolean = getUserTelemetrySetting(),
         private readonly instrumentationKey = "a5e759de-afbd-4f36-a9c9-2fc95385683b",
     ) {
         const packageJson = context.extension.packageJSON;
@@ -27,14 +28,43 @@ export class ExtensionReporter extends TelemetryReporter {
         super(extId, extVersion, instrumentationKey);
     }
 
+
     public sendTelemetryEvent(
         eventName: string,
         properties?: TelemetryEventProperties | undefined,
         measurements?: TelemetryEventMeasurements | undefined,
     ) {
+        if (!this.enableTelemetry) { return; }
         if (this.verbose) {
             console.log(`Telemetry event: ${eventName}`, properties, measurements);
         }
         super.sendTelemetryEvent(eventName, properties, measurements);
     }
+}
+
+function getUserTelemetrySetting() {
+    const globalTelemetryIsEnabled: (
+        "all" | "error" | "crash" | "off" | undefined
+    ) = vscode.workspace.getConfiguration('telemetry').get('telemetryLevel');
+    if (!(globalTelemetryIsEnabled === "all" || globalTelemetryIsEnabled === undefined)) {
+        // our extension only collects "Usage Data", allowed by "all" only
+        return false;
+    }
+
+    const globalIsTelemetryEnabled: (
+        boolean | undefined
+    ) = vscode.workspace.getConfiguration('telemetry').get('isTelemetryEnabled');
+    if (globalIsTelemetryEnabled === false) { return false; }
+
+    const globalOnDidChangeTelemetryEnabled: (
+        boolean | undefined
+    ) = vscode.workspace.getConfiguration('telemetry').get('onDidChangeTelemetryEnabled');
+    if (globalOnDidChangeTelemetryEnabled === false) { return false; }
+
+    const extensionIsTelemetryEnabled: (
+        boolean | undefined
+    ) = vscode.workspace.getConfiguration('telemetry.geriyocoImageGallery').get('isTelemetryEnabled');
+    if (extensionIsTelemetryEnabled === false) { return false; }
+
+    return true;
 }
