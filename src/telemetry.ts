77d2a79d2c61cf495e49b88857d161efa5163468
Fanurs Teh <fyanurs@gmail.com>
2022-09-17 16:51:37 -0400

    :technologist: Adjustable verbose for sendTelemetryEvent()

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -1,5 +1,7 @@
 import * as vscode from 'vscode';
-import TelemetryReporter from '@vscode/extension-telemetry';
+import TelemetryReporter, {
+    TelemetryEventMeasurements, TelemetryEventProperties
+} from '@vscode/extension-telemetry';
 
 export let reporter: ExtensionReporter;
 
@@ -16,11 +18,23 @@ export function deactivate() {
 export class ExtensionReporter extends TelemetryReporter {
     constructor(
         context: vscode.ExtensionContext,
+        public verbose = false, // true for development; false for production
         private readonly instrumentationKey = "a5e759de-afbd-4f36-a9c9-2fc95385683b",
     ) {
         const packageJson = context.extension.packageJSON;
         const extId = packageJson.publisher + '.' + packageJson.name;
         const extVersion = packageJson.version;
         super(extId, extVersion, instrumentationKey);
+    }
+
+    public sendTelemetryEvent(
+        eventName: string,
+        properties?: TelemetryEventProperties | undefined,
+        measurements?: TelemetryEventMeasurements | undefined,
+    ) {
+        if (this.verbose) {
+            console.log(`Telemetry event: ${eventName}`, properties, measurements);
+        }
+        super.sendTelemetryEvent(eventName, properties, measurements);
     }
 }
