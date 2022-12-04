bd3add6a65ec6ff25d447040790619a164f2e995
Fanurs Teh <fyanurs@gmail.com>
2022-12-04 00:55:29 -0500

    :test_tube: First test draft using chai

---
+++ src/test/suite/viewer.test.ts
@@ -0,0 +1,15 @@
+import vscode from 'vscode';
+
+import * as utils from '../../utils';
+
+suite('GeriYoco.vscode-image-gallery: Viewer Test Suite', () => {
+	vscode.window.showInformationMessage('Viewer Test Suite started.');
+
+	// test('viewer', async () => {
+	// 	await vscode.commands.executeCommand(
+	// 		'vscode.open',
+	// 		vscode.Uri.file(utils.getCwd() + 'folder1/image1.jpg')
+	// 	);
+
+	// });
+});
